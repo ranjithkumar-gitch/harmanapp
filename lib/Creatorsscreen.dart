f492cd50ec3045a09d6d2bbdd43e5d1af75982ca
M Chandrashekar <chandumitty@gmail.com>
2025-12-11 00:56:27 +0530

    CarouselSlider SLIDER added on creatorscreen

--- lib/Creatorsscreen.dart
+++ lib/Creatorsscreen.dart
@@ -1,7 +1,9 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:harmanapp/helper.dart';
 import 'package:harmanapp/models/user_post_model.dart';
+import 'package:rive/rive.dart';
 import 'package:scroll_snap_list/scroll_snap_list.dart';
 
 class CreatorsScreen extends StatefulWidget {
@@ -62,7 +64,7 @@ class _CreatorsScreenState extends State<CreatorsScreen> {
 
               /// SILVER SECTION
               _title("Silver", Colors.grey),
-              _horizontalList(
+              _horizontalSILVERList(
                 posts.where((p) => p.hasStory).toList(),
                 gradient: const [
                   CupertinoColors.systemGrey,
@@ -74,7 +76,7 @@ class _CreatorsScreenState extends State<CreatorsScreen> {
 
               /// BRONZE SECTION
               _title("Bronze", CupertinoColors.systemBrown),
-              _horizontalList(
+              _horizontalBRONZEList(
                 posts.where((p) => p.hasStory).toList(),
                 gradient: const [
                   CupertinoColors.systemBrown,
@@ -107,152 +109,312 @@ class _CreatorsScreenState extends State<CreatorsScreen> {
     required List<Color> gradient,
   }) {
     return SizedBox(
-      height: 120,
-      child: ScrollSnapList(
-        listViewPadding: EdgeInsetsGeometry.all(0),
-        margin: EdgeInsets.symmetric(horizontal: 5),
-        scrollPhysics: BouncingScrollPhysics(),
-        curve: Curves.slowMiddle,
-        selectedItemAnchor: SelectedItemAnchor.MIDDLE,
-        focusOnItemTap: true,
-        duration: 500,
-        onItemFocus: (index) {},
-        dynamicItemSize: true,
-        itemSize: 100,
-        itemBuilder: (_, i) {
-          return SubscriberSquare(user: list[i], gradient: gradient);
-        },
-        itemCount: list.length,
-        reverse: false,
+      height: 135,
+      child: CarouselSlider(
+        items: list.map((i) {
+          return Builder(
+            builder: (BuildContext context) {
+              return Container(
+                color: Colors.black,
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            shape: BoxShape.rectangle,
+                            color: CupertinoColors.white,
+                            image: DecorationImage(
+                              image: AssetImage(
+                                'assets/sources/profiles/${i.profileImage}',
+                              ),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      i.name,
+                      style: const TextStyle(
+                        color: CupertinoColors.white,
+                        fontSize: 12,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              );
+              //SizedBox(height: 6),
+            },
+          );
+        }).toList(),
+        options: CarouselOptions(
+          height: 355,
+          autoPlay: true,
+          autoPlayInterval: Duration(seconds: 3),
+          autoPlayAnimationDuration: Duration(milliseconds: 950),
+          enlargeCenterPage: true,
+          aspectRatio: 16 / 9,
+          viewportFraction: 0.3,
+          animateToClosest: true,
+          enableInfiniteScroll: true,
+        ),
       ),
     );
   }
-}
 
-class SubscriberSquare extends StatelessWidget {
-  final UserPostModel user;
-  final List<Color> gradient;
-  final double size;
-  final bool hideName;
-
-  const SubscriberSquare({
-    super.key,
-    required this.user,
-    required this.gradient,
-    this.size = 100,
-    this.hideName = false,
-  });
-
-  @override
-  Widget build(BuildContext context) {
+  Widget _horizontalSILVERList(
+    List<UserPostModel> list, {
+    required List<Color> gradient,
+  }) {
     return SizedBox(
-      width: size,
-      child: Card(
-        color: Colors.black,
-        elevation: 12,
-        child: ClipRRect(
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                height: size - 5,
-                width: size - 5,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: LinearGradient(colors: gradient),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(2),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: CupertinoColors.white,
-                      image: DecorationImage(
-                        image: AssetImage(
-                          'assets/sources/profiles/${user.profileImage}',
+      height: 135,
+      child: CarouselSlider(
+        items: list.map((i) {
+          return Builder(
+            builder: (BuildContext context) {
+              return Container(
+                color: Colors.black,
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            shape: BoxShape.rectangle,
+                            color: CupertinoColors.white,
+                            image: DecorationImage(
+                              image: AssetImage(
+                                'assets/sources/profiles/${i.profileImage}',
+                              ),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
                         ),
-                        fit: BoxFit.cover,
                       ),
                     ),
-                  ),
+                    Text(
+                      i.name,
+                      style: const TextStyle(
+                        color: CupertinoColors.white,
+                        fontSize: 12,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
                 ),
-              ),
+              );
               //SizedBox(height: 6),
-              if (!hideName)
-                Text(
-                  user.name,
-                  style: const TextStyle(
-                    color: CupertinoColors.white,
-                    fontSize: 12,
-                  ),
-                  overflow: TextOverflow.ellipsis,
+            },
+          );
+        }).toList(),
+        options: CarouselOptions(
+          height: 355,
+          autoPlay: true,
+          autoPlayInterval: Duration(seconds: 6),
+          autoPlayAnimationDuration: Duration(milliseconds: 805),
+          enlargeCenterPage: true,
+          aspectRatio: 16 / 9,
+          viewportFraction: 0.3,
+          animateToClosest: true,
+          enableInfiniteScroll: true,
+        ),
+      ),
+    );
+  }
+
+  Widget _horizontalBRONZEList(
+    List<UserPostModel> list, {
+    required List<Color> gradient,
+  }) {
+    return SizedBox(
+      height: 135,
+      child: CarouselSlider(
+        items: list.map((i) {
+          return Builder(
+            builder: (BuildContext context) {
+              return Container(
+                color: Colors.black,
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            shape: BoxShape.rectangle,
+                            color: CupertinoColors.white,
+                            image: DecorationImage(
+                              image: AssetImage(
+                                'assets/sources/profiles/${i.profileImage}',
+                              ),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      i.name,
+                      style: const TextStyle(
+                        color: CupertinoColors.white,
+                        fontSize: 12,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
                 ),
-            ],
-          ),
+              );
+              //SizedBox(height: 6),
+            },
+          );
+        }).toList(),
+        options: CarouselOptions(
+          height: 355,
+          autoPlay: true,
+          autoPlayInterval: Duration(seconds: 5),
+          autoPlayAnimationDuration: Duration(milliseconds: 950),
+          enlargeCenterPage: true,
+          aspectRatio: 16 / 9,
+          viewportFraction: 0.3,
+          animateToClosest: true,
+          enableInfiniteScroll: true,
         ),
       ),
     );
   }
 }
 
-class SubscriberCircle extends StatelessWidget {
-  final UserPostModel user;
-  final List<Color> gradient;
-  final double size;
-  final bool hideName;
+// class SubscriberSquare extends StatelessWidget {
+//   final UserPostModel user;
+//   final List<Color> gradient;
+//   final double size;
+//   final bool hideName;
 
-  const SubscriberCircle({
-    super.key,
-    required this.user,
-    required this.gradient,
-    this.size = 80,
-    this.hideName = false,
-  });
+//   const SubscriberSquare({
+//     super.key,
+//     required this.user,
+//     required this.gradient,
+//     this.size = 100,
+//     this.hideName = false,
+//   });
 
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: size,
-      child: Column(
-        children: [
-          Container(
-            height: size - 10,
-            width: size - 10,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              gradient: LinearGradient(colors: gradient),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(2),
-              child: Container(
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: CupertinoColors.white,
-                  image: DecorationImage(
-                    image: AssetImage(
-                      'assets/sources/profiles/${user.profileImage}',
-                    ),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          if (!hideName)
-            Text(
-              user.name,
-              style: const TextStyle(
-                color: CupertinoColors.white,
-                fontSize: 12,
-              ),
-              overflow: TextOverflow.ellipsis,
-            ),
-        ],
-      ),
-    );
-  }
-}
+//@override
+// Widget build(BuildContext context) {
+//   return SizedBox(
+//     width: size,
+//     child: Card(
+//       color: Colors.black,
+//       elevation: 12,
+//       child: ClipRRect(
+//         borderRadius: const BorderRadius.all(Radius.circular(10)),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.start,
+//           children: [
+//             Container(
+//               height: size - 5,
+//               width: size - 5,
+//               decoration: BoxDecoration(
+//                 shape: BoxShape.circle,
+//                 //gradient: LinearGradient(colors: gradient),
+//               ),
+//               child: Padding(
+//                 padding: const EdgeInsets.all(2),
+//                 child: Container(
+//                   decoration: BoxDecoration(
+//                     shape: BoxShape.circle,
+//                     color: CupertinoColors.white,
+//                     image: DecorationImage(
+//                       image: AssetImage(
+//                         'assets/sources/profiles/${user.profileImage}',
+//                       ),
+//                       fit: BoxFit.cover,
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             //SizedBox(height: 6),
+//             if (!hideName)
+//               Text(
+//                 user.name,
+//                 style: const TextStyle(
+//                   color: CupertinoColors.white,
+//                   fontSize: 12,
+//                 ),
+//                 overflow: TextOverflow.ellipsis,
+//               ),
+//           ],
+//         ),
+//       ),
+//     ),
+//   );
+// }
+//}
+
+// class SubscriberCircle extends StatelessWidget {
+//   final UserPostModel user;
+//   final List<Color> gradient;
+//   final double size;
+//   final bool hideName;
+
+//   const SubscriberCircle({
+//     super.key,
+//     required this.user,
+//     required this.gradient,
+//     this.size = 80,
+//     this.hideName = false,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SizedBox(
+//       width: size,
+//       child: Column(
+//         children: [
+//           Container(
+//             height: size - 10,
+//             width: size - 10,
+//             decoration: BoxDecoration(
+//               shape: BoxShape.circle,
+//               //gradient: LinearGradient(colors: gradient),
+//             ),
+//             child: Padding(
+//               padding: const EdgeInsets.all(2),
+//               child: Container(
+//                 decoration: BoxDecoration(
+//                   shape: BoxShape.circle,
+//                   color: CupertinoColors.white,
+//                   image: DecorationImage(
+//                     image: AssetImage(
+//                       'assets/sources/profiles/${user.profileImage}',
+//                     ),
+//                     fit: BoxFit.cover,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//           if (!hideName)
+//             Text(
+//               user.name,
+//               style: const TextStyle(
+//                 color: CupertinoColors.white,
+//                 fontSize: 12,
+//               ),
+//               overflow: TextOverflow.ellipsis,
+//             ),
+//         ],
+//       ),
+//     );
+//   }
+//}
 
 class DataSearch extends SearchDelegate<String> {
   final List<UserPostModel> listSubscribe;
